4c7d000c0e314c4bb9d15eb2a5c15d72df271fbc
mike <michael.hallam3@gmail.com>
2021-08-27 22:14:57 +0100

    added container classes to abstract away db queries - yet to test if they work yet. Mostly for ease of use with pycharm

---
+++ receiveemails/containers.py
@@ -0,0 +1,15 @@
+class ID:
+    id: int
+
+
+class Account:
+    local: str
+    domain: str
+
+
+class Email:
+    id: ID
+    sender: Account
+    receivers: [(ID, Account)]
+    subject: str
+    body: str
