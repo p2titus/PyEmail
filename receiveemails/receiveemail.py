0832085faac4328942885f3117d4dc1847813d42
mike <michael.hallam3@gmail.com>
2021-08-28 21:25:13 +0100

    copied key loader code from sendemail to receiveemail also added documentation to receiveemail

--- receiveemails/receiveemail.py
+++ receiveemails/receiveemail.py
@@ -1,4 +1,32 @@
 import poplib
 
-class EmailReceiver:
 
+"""
+Recall POP downloads email from a (remote) server and deletes them from the server once received
+By contrast, IMAP caches emails, allows you to perform actions, uploads the actions then deletes the cache
+As we store emails in this server, POP is the correct choice of protocol
+HOWEVER, you may choose to use IMAP instead. If this is the case, you should only need to reconfigure the message 
+receiving in this class
+No other class should receive email except by calling methods from this class
+"""
+
+
+class EmailReceiver:
+    __DEBUG_LVL = 0  # use 0 for prod
+
+    # for an explanation, see the almost identical code in userinput/sendemail.py
+    def __init__(self):
+        KEYS_NAME = '../keys.json'
+        ks = self.__load_keys(KEYS_NAME)
+        accs = []
+        for k in ks:
+            accs.append((k['addr'], k['type'], k['keys']))
+        self.__ks = accs
+
+    @staticmethod
+    def __load_keys(keys_file_name):
+        from json import load
+        f = open(keys_file_name)
+        x = load(f)
+        f.close()
+        return x
