6ae3a07ad0850bc58873fc1609e794883195f76e
mike <michael.hallam3@gmail.com>
2021-09-05 20:39:07 +0100

    wrote a unit test for load keys

--- controller/load_keys.py
+++ controller/load_keys.py
@@ -1,14 +1,22 @@
-def load_account_details(keys_file_name="keys.json"):
-    fname = "../%s", keys_file_name
+import os
+
+
+def load_account_details(*args, keys_file_name="keys.json"):
+    fname = "../%s" % keys_file_name
     ks = __load_keys(fname)
     acc = []
-    for k in ks:
-        acc.append((k['addr'], k['type'], k['keys']))
+    if ks is not None:
+        for k in ks:
+            x = ks[k]
+            acc.append((x['addr'], x['type'], x['keys']))
     return acc
 
 
 def __load_keys(fname):
     from json import load
-    with open(fname) as f:
-        x = load(f)
+    if os.path.exists(fname) and os.stat(fname).st_size > 0:
+        with open(fname) as f:
+            x = load(f)
+    else:
+        x = None
     return x
--- controller/tests.py
+++ controller/tests.py
@@ -4,37 +4,55 @@ import json
 
 
 class LoadKeysTest(unittest.TestCase):
+    from load_keys import load_account_details
     prod_keys = None
     __KEYS_FNAME = 'test.json'
-    __TEST_KEYS = LoadKeysTest.__gen_keys()
+    __TEST_KEYS: (str, str, str) = None
 
-    def tearDown(self) -> None:
+    @classmethod
+    def tearDownClass(cls) -> None:
         # we return the prod keys after a test
         # although if you're saving your prod keys in a file called test.json, you kinda deserve it
-        if self.prod_keys is not None:
-            fname = self.__KEYS_FNAME
+        if cls.prod_keys is not None:
+            fname = cls.__KEYS_FNAME
             if os.path.exists(fname):
                 os.remove(fname)
-            permissions = 'w'
-            with open(fname, permissions) as f:
-                json.dump(self.prod_keys, f)
+            if cls.prod_keys is not None:
+                with open(fname, 'w') as f:
+                    json.dump(cls.prod_keys, f)
 
     @staticmethod
     def __gen_keys():
+        # return 'test', 'example.com', 'testpwd'
         return dict(test=dict(addr="test", type="example.com", keys="testpwd"))
 
     @classmethod
-    def setUp(cls) -> None:
-        def_ks = cls.__TEST_KEYS
-        fname = cls.__KEYS_FNAME
-        perm = 'rw'
-        with open(fname, perm) as f:
-            if os.path.exists(fname):
+    def setUpClass(cls) -> None:
+        def_ks = cls.__gen_keys()
+        cls.__TEST_KEYS = cls.__extract_keys(def_ks)
+        fname = '../%s' % cls.__KEYS_FNAME
+        if os.path.exists(fname) and os.stat(fname).st_size > 0:
+            with open(fname, 'r') as f:
                 cls.prod_keys = json.load(f)
-            json.dumps(def_ks)
+        with open(fname, 'w') as f:
+            json.dump(def_ks, f, indent=4)
+        print(cls.__TEST_KEYS)
+        print(cls.__KEYS_FNAME)
 
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    @staticmethod
+    def __extract_keys(ds):
+        acc = []
+        if len(ds) > 1:
+            for d in ds:
+                acc.append((d['addr'], d['type'], d['keys']))
+        elif len(ds) == 1:
+            xs = ds['test']  # cheesed it
+            acc = [(xs['addr'], xs['type'], xs['keys'])]
+        return acc
+
+    def test_basic(self):
+        ks = self.load_account_details(keys_file_name=self.__KEYS_FNAME)
+        self.assertEqual(ks, self.__TEST_KEYS)
 
 
 if __name__ == '__main__':
